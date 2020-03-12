808a1471c4f2063c744d8209600a30bd134f6d1c
Inga Danek <danekinga@gmail.com>
2020-03-12 19:29:00 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let activeElement = 0;
+const timeChange = 4000;
+const colorImgHtml = document.querySelector('.color');
+const grayImgHtml = document.querySelector('.gray');
+const h1Html = document.querySelector('.member h1');
+const h2Html = document.querySelector('.member h2');
+
+const colorImages = ['img/s1.png', 'img/s2.png', 'img/s3.png'];
+const grayImages = ['img/s1a.png', 'img/s2a.png', 'img/s3a.png'];
+const names = ['Kate Smith', 'Robert Johnson', 'John Miller'];
+const professions = ['JS Programmer', 'UX/UI Designer', 'Front-End Developer'];
+
+function changeElement() {
+    activeElement++;
+    if (activeElement == colorImages.length) {
+        activeElement = 0;
+    }
+    colorImgHtml.src = colorImages[activeElement];
+    grayImgHtml.src = grayImages[activeElement];
+    h1Html.textContent = names[activeElement];
+    h2Html.textContent = professions[activeElement];
+
+}
+
+setInterval(changeElement, timeChange)
